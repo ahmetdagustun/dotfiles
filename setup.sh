11c956a9a1da01857801565a1630429b8bfbc7d6
Ahmet Dagustun <ahmetdagustun2007tl@gmail.com>
2026-04-01 19:17:18 +0300

    Added Brewfile, zshrc, and setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -sf ~/dotfiles/aerospace/.aerospace.toml ~/.aerospace.toml
+ln -sf ~/dotfiles/ghostty/config ~/.config/ghostty/config
+ln -sf ~/dotfiles/borders/bordersrc ~/.config/borders/bordersrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+echo "Rice installed successfully!"
